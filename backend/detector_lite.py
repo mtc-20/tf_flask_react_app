7ff6fb3892bdecb14d01b47309a3a0c9b74c5acd
mtc-20 <mamenthomas.c@gmail.com>
2022-03-19 02:11:48 +0100

    still troubleshooting

--- backend/detector_lite.py
+++ backend/detector_lite.py
@@ -179,12 +179,13 @@ def getDetections(image_file, interpreter):
 	scores = interpreter.get_tensor(output_details[2]['index'])[0]
 	num = interpreter.get_tensor(output_details[3]['index'])[0]
 
-	overlaid = viz_bboxes_and_labels(img_input, boxes.copy(), labels.astype(np.int64), scores.copy(), labels_dict)
+	overlaid = viz_bboxes_and_labels(img_input, boxes.copy(), labels.astype(np.int64).copy(), scores.copy(), labels_dict)
 	del img_arr
 	gc.collect()
 	return overlaid
 
 def overlayImage(image_file):
+	interpreter = lite.Interpreter(model_path)
 	overlay_img = getDetections(image_file, interpreter)
 	# overlay_img = Image.fromarray(overlay_arr.astype(np.uint8))
 	file_obj = BytesIO()
